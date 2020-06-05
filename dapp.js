af3883849714c4b484db6e02a6c1c396ef9b605d
nahom80 <nahom80@yahoo.com>
2020-06-05 19:44:17 -0400

    Update dapp.js

--- dapp.js
+++ dapp.js
@@ -79,7 +79,7 @@ const dApp = {
 		
 
 		<span id="dapp-name" class="card-title">${token.name} - ${token.tokenId} </span>
-		<!-- <span id="dapp-tokenid" class="card-title">${token.tokenId}</span> -->
+		
 	                
 		</div>
                 <div class="card-action">
